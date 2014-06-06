978b03ba4fcdc8724c5f98cf650c3b30552fff09
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-06-06 20:12:08 +0200

    added button showing the existance of MicroData

--- data/microdata-extractor.js
+++ data/microdata-extractor.js
@@ -1,11 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  if(document.getItems().length > 0) {
-    console.log(document.title + " contains micro data");
-    var semanticsDisclaimer = document.createElement("div");
-    semanticsDisclaimer.innerHTML =
-      '<div style="position:fixed; bottom: 1em; right: 1em; background: #ccc; border: 1px solid #555">' +
-        'Microdata embeded'+
-      '</div>';
-    document.body.appendChild(semanticsDisclaimer);
-  }
+  var containsMicroData = document.getItems().length > 0;
+  self.port.emit("reportMicroData", containsMicroData);
 });
