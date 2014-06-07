3244d525d33cd3c26301eb04c54088a07b4d9e26
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-06-07 05:31:55 +0200

    extract MicroData as JSON & log it to the console

--- data/microdata-extractor.js
+++ data/microdata-extractor.js
@@ -1,4 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var containsMicroData = document.getItems().length > 0;
-  self.port.emit("reportMicroData", containsMicroData);
+"use strict";
+
+window.addEventListener("unload", function() {
+  self.port.emit("unload");
+});
+
+function extractMicroDataFromItem(item) {
+  var data = {};
+  if(item.itemType.length > 0) {
+    var extractedTypes = [];
+    for(var typeNr = 0; typeNr < item.itemType.length; typeNr++) {
+      extractedTypes.push(item.itemType[typeNr]);
+    }
+    data['type'] = extractedTypes;
+  }
+  if(item.itemId !== "") {
+    data['id'] = item.itemId;
+  }
+  var extractedProperties = {};
+  for(var propertyNr = 0; propertyNr < item.properties.names.length; propertyNr++) {
+    var propName = item.properties.names[propertyNr];
+    var valueNodes = item.properties[propName];
+    var extractedValues = [];
+    for(var valueNr = 0; valueNr < valueNodes.length; valueNr++) {
+      var value = valueNodes[valueNr].itemValue
+      if(value instanceof Element) {
+        value = extractMicroDataFromItem(value);
+      }
+      extractedValues.push(value);
+    }
+    extractedProperties[propName] = extractedValues;
+  }
+  data['properties'] = extractedProperties;
+  return data;
+}
+
+function extractMicroData() {
+  var extractedItems = [];
+  var items = document.getItems();
+  for(var itemNr = 0; itemNr < items.length; itemNr++) {
+    var item = items[itemNr];
+    extractedItems.push(extractMicroDataFromItem(item));
+  }
+  return {items: extractedItems};
+}
+
+self.port.on("extractMicroData", function() {
+  var microData = extractMicroData();
+  self.port.emit("returnMicroData", microData);
 });
