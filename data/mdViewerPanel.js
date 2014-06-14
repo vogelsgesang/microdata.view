8f8c9da4923ce0ff86df610294fcc43fbc2d8cc6
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-06-14 04:23:44 +0200

    display extracted MicroData in panel

--- data/mdViewerPanel.js
+++ data/mdViewerPanel.js
@@ -1,3 +1,34 @@
-window.addEventListener("load", function() {
-  document.getElementById("json").textContent = "{\"data\": \"micro\", \"TODO\": \"implement\"}";
-});
+"use strict";
+//hides all pages and only shows the specified one
+function showPage(name) {
+  var pages = ["loading", "no-microdata", "microdata"]
+  for(var i = 0; i < pages.length; i++) {
+    document.getElementById(pages[i]).style.display = "none";
+  }
+  document.getElementById(name).style.display = "block";
+}
+showPage("loading");
+
+//updates the view
+self.port.on("updateMicroData", displayMicroData);
+function displayMicroData(microData) {
+  var items = microData.items;
+  if(items.length > 0) {
+    document.getElementById("json").textContent = JSON.stringify(items, null, 2);
+    showPage("microdata");
+  } else {
+    showPage("no-microdata");
+  }
+}
+
+//asks for the extraction of microdata from the current web page
+function reloadMicroData() {
+  showPage("loading");
+  self.port.emit("extractMicroData");
+}
+
+//reload microdata on "show" event
+self.port.on("show", reloadMicroData);
+//a direct call is necessary since (on the first load) we might have
+//already missed the "show" message.
+reloadMicroData();
